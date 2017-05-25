e4ef8f8d17630150cf87391113b0f1ed0a0f8499
hritwik <hbhritik@gmail.com>
2017-05-25 13:37:28 +0530

    done!

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //create player spawn point
+        //this is where our player will tart when the game is played
+        //player = gameobject & gameobject = transform 
+
+        transform.position = new Vector3(-2, 3, 0);	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
